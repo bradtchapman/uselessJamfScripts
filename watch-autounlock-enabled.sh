096de1a4502ada295556c5bd5f3425654788039c
bradtchapman <32527656+bradtchapman@users.noreply.github.com>
2021-05-08 22:32:18 -0700

    Update watch-autounlock-enabled.sh
    Added comments to explain the script

--- watch-autounlock-enabled.sh
+++ watch-autounlock-enabled.sh
@@ -1,10 +1,22 @@
 #!/bin/zsh
 
+# Variable initialization and declarations
+# the 'pairing-records' file contains key values when
+# when AutoUnlock is enabled.  If not, the file is devoid of keys.
+
+
 isPairingActive=""
 pairingRecordsPath="/Library/Sharing/AutoUnlock/pairing-records.plist"
 unlockingWithWatch=()
 
 listOfUsers=($(/bin/ls -1 /Users/ ))
+
+# Check all users.  If the specific pairing-record file isn't found, nothing happens.
+# The execution time of this script is fast enough not to warrant further optimization.
+# If a pairing record is found and not empty, record the user in the array.
+#
+# NOTE: although Apple only supports one user unlocking with a Watch today,
+# The use of an array is my way of future-proofing in case Apple changes their mind :-)
 
 for i in ${listOfUsers[@]};
 do
@@ -19,6 +31,8 @@ do
                 fi
         fi
 done
+
+# Print the results for a Jamf Extension Attribute
 
 if [[ ${#unlockingWithWatch[@]} == 0 ]]
 then
