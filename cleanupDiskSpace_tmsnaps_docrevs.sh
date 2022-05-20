f3261d4cd5558fd0619b3be335f2ea30259d00b2
bradtchapman <32527656+bradtchapman@users.noreply.github.com>
2022-05-20 15:05:25 -0700

    Added 'are you sure?' script for end user
    Just to make sure they are REALLY REALLY sure they want to clean up these two items.

--- cleanupDiskSpace_tmsnaps_docrevs.sh
+++ cleanupDiskSpace_tmsnaps_docrevs.sh
@@ -5,7 +5,20 @@ jh="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelp
 
 # Time Machine snapshots are available on macOS 10.13.6 and higher; however,
 # on macOS 10.14.6 and below, the 'deletelocalsnapshots' verb requires specifying
-# each snapshot.  Therefore, the below will not work on those older OS'es.
+# each snapshot.  Therefore, the below will not work in older OS'es.
+
+"$jh" -windowType utility -title "Self Service Disk Cleanup" \
+-heading "Are you sure you want to proceed?" \
+-description "I have read the warnings about this tool in Self Service.  I promise I have made backups of my most important data files.  I understand that this tool may result in data loss." \
+-icon "/System/Library/CoreServices/Dock.app/Contents/Resources/trashfull@2x.png" -iconSize 128 \
+-button1 "I AGREE" -button2 "CANCEL" -defaultButton 2 -cancelButton 2 -timeout 30 -countdown -alignCountdown right
+cleanupChoice=$?
+
+case $cleanupChoice in
+	0) echo "User agreed to clean up." ;;
+    2) echo "User cancelled.  Aborting..."; exit 0 ;;
+    *) echo "Something else went wrong.  Aborting..."; exit 0 ;;
+esac
 
 if [[ $osVersion -ge 19 ]]
 then
