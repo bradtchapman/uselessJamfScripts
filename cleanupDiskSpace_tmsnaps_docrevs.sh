266d23000ea8ef7bd20ba897452a57a804b4eedd
bradtchapman <32527656+bradtchapman@users.noreply.github.com>
2022-05-20 12:45:27 -0700

    Create cleanupDiskSpace_tmsnaps_docrevs.sh

---
+++ cleanupDiskSpace_tmsnaps_docrevs.sh
@@ -0,0 +1,43 @@
+#!/bin/zsh
+
+osVersion=$(sw_vers -BuildVersion | cut -c 1,2)
+jh="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
+
+# Time Machine snapshots are available on macOS 10.13.6 and higher.
+
+if [[ $osVersion -ge 17 ]]
+then
+	echo "Stopping Time Machine and deleting snapshots..."
+    
+    /usr/bin/tmutil stopbackup > 2>&1
+    sleep 2
+	/usr/bin/tmutil deletelocalsnapshots / 2>&1
+fi
+
+# DocumentRevisions-V100 folder is in two different places depending on the OS.
+# In Mojave and below, it's in the root folder directly.
+# In Catalina+ it is moved to the "Data" volume and must be accessed via /System/Volumes/Data.
+
+if [[ $osVersion -lt 19 ]]
+then
+	docRevisionsFolder="/.DocumentRevisions-V100"
+else
+	docRevisionsFolder="/System/Volumes/Data/.DocumentRevisions-V100"
+fi
+
+echo "Clearing the DocumentRevisions folder..."
+/bin/rm -rf "$docRevisionsFolder"/* 2>&1
+
+echo "The computer MUST be restarted now."
+
+"$jh" -windowType utility -title "Self Service Disk Cleanup" \
+-heading "Disk cleanup complete.  Restart required." \
+-description "The cleanup actions require your Mac to be restarted immediately.  Please save your work and prepare to reboot." \
+-icon "/System/Library/CoreServices/Dock.app/Contents/Resources/trashfull@2x.png" -iconSize 128 \
+-timeout 120 -countdown -alignCountdown right -button1 "OK"
+
+/bin/launchctl reboot apps
+
+sleep 5
+
+/bin/launchctl reboot system
